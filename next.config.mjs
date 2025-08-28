fd5d23d0199e8e75202445b2779392871d911a18
tanvi874 <tanvi.bhasin@hatrotech.com>
2025-08-28 16:52:22 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -32,7 +32,7 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://np.in8.cdn-alpha.com" }, // Be sure to set this to your Webflow domain
+          { key: "Access-Control-Allow-Origin", value: "https://np.in8.cdn-alpha.com/company/" }, // Be sure to set this to your Webflow domain
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ],
